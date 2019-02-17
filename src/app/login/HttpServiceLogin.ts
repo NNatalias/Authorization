8aa880ccafce9a65e7a82564ceb0cf32bd84c884
NNatalias <35404842+NNatalias@users.noreply.github.com>
2019-02-17 14:09:14 +0200

    Update HttpServiceLogin.ts

--- src/app/login/HttpServiceLogin.ts
+++ src/app/login/HttpServiceLogin.ts
@@ -1,12 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from './login';
 @Injectable()
 export class HttpServiceLogin {
 
     constructor(private http: HttpClient) { }
 
-    postData( user: User ) {
+    postData( user) {
 
         const body = {email: user.email, password: user.password};
         return this.http.post('http://1435247.mysitesi.web.hosting-test.net/phpservice/loginusers.php', body);
